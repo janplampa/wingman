4d946836941e560ff789494e169b03f525448e86
Jan <jan.lampa@iress.com>
2021-07-30 23:06:40 +1000

    write result to file

--- compareTwoFiles.sh
+++ compareTwoFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Below compares two files 
+#Below compares two files and output the result into another file
 
 declare -a tapes
 
@@ -10,13 +10,15 @@ readarray file1 < "file1.txt"
 echo ${#file[@]} # counts the # of lines in this file
 echo ${#file1[@]} # counts the # of lines in this file
 
+touch result.txt
+
 for i in ${file[@]} 
 do
     for j in ${file1[@]} 
     do
         if [[ *"$i"* == *"$j"* ]]
         then
-            echo $i
+            echo $i >> result.txt
         fi
     done
 done
