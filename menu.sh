9b1f12a11903b80c04c43fd3ce434094a0622ce6
Jan <jan.lampa@iress.com>
2021-08-06 16:14:44 +1000

    menu script

---
+++ menu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "What would you like to make today?"
+echo "1. Dinner"
+echo "2. Bake"
+
+read menu;
+
+case $menu in 
+    1)
+        echo "What would you like to make dinner?"
+        printf "1. Beef beans stir fry \n2. Prok Adobo \n"
+        read dinner;
+    ;;
+    2) 
+        echo "what would you like to bake?"
+        printf "1. Coockies \n2. Banana bread"
+    ;;
+esac
